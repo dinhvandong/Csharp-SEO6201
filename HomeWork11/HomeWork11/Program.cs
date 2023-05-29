7798113adf5cf0772c7246b26e0a934b9656d2f5
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-05-29 14:40:24 +0700

    HomeWork11

---
+++ HomeWork11/HomeWork11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+
+            long sum = 0;
+            Console.WriteLine("Nhap n:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            // (1) + (1.2) + (1.2.3) + (1.2.3.4) + ....(1.2.3.4...n)
+
+            // 1.2.3.4...n = n giai thua
+            long x = 1;
+
+            for(int i = 1;i<=n ;i++)
+            {
+                x = x * i; // 1*2*3*....(n-1) * n = n giai thua
+                sum = sum + x;
+            }
+
+            Console.WriteLine("Result is "+ sum);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
--- HomeWork3/HomeWork3/Program.cs
+++ HomeWork3/HomeWork3/Program.cs
@@ -13,14 +13,14 @@ namespace HomeWork3
             //sum =   1/1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6 + 1/7 + ..... 1/n
             int n;
 
-            double sum = 0;
+            float sum = 0;
 
             Console.WriteLine("Nhap vao gia tri n");
             n = Convert.ToInt32(Console.ReadLine());
 
             for(int i = 1; i <= n; i++)
             {
-                sum = sum + (1.0 / i); // 1.0 is float => 1.0/i is Float 
+                sum = sum + (1.0f / i); // 1.0 is float => 1.0/i is Float 
 
             }
 
