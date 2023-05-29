2d3a8939e6f6b46d96baaba678b2281f8a91e4c2
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-05-29 12:50:20 +0700

    Bai 1

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            int sum = 0;
+
+            Console.WriteLine("Nhap gia tri N:");
+            // Console.ReadLine() == nhan input tu keyboard  is String (Chuoi ky tu) 
+
+            n = Convert.ToInt32(Console.ReadLine());// Convert String to Int32
+
+            // 1+ 2 + 3 + ...n
+
+            for(int i = 1; i <= n; i++)
+            {
+
+                sum = sum + i;
+
+            }
+            Console.WriteLine("Sum is "+ sum);
+
+            Console.ReadLine();
+        }
+    }
+}
