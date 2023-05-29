e9cef8619e7e2161df10c24a8a89d014c7a91f94
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-05-29 13:11:34 +0700

    Bai 2

---
+++ HomeWork2/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1^2 + 2^2 + ...n^2
+
+            int n;
+            long sum = 0;
+
+            Console.WriteLine("Nhap vao gia tri n:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1;i<= n;i++)
+            {
+                sum = sum + i*i;
+            }
+            Console.WriteLine("Sum is " + sum);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
